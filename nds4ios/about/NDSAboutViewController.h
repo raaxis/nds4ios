fe6c6b1701bd5fba1e73ebb8c32255570145c462
Michael Zhang <michael_zhang8000@yahoo.ca>
2013-07-16 09:05:55 -0700

    Fixed DeSmuME text and DS Team

--- nds4ios/about/NDSAboutViewController.h
+++ nds4ios/about/NDSAboutViewController.h
@@ -3,7 +3,7 @@
 //  nds4ios
 //
 //  Created by Developer on 7/8/13.
-//  Copyright (c) 2013 DS Team. All rights reserved.
+//  Copyright (c) 2013 InfiniDev. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
