4d36c7fc43c3e88ae811c4b6382bbbb8e2d9cfdb
Malvix <michaelzhang810@gmail.com>
2013-08-14 15:20:02 -0700

    Added Winocm to "About Us"

--- nds4ios/about/NDSAboutViewController.m
+++ nds4ios/about/NDSAboutViewController.m
@@ -49,7 +49,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSArray *screenNames = @[@"angelXwind", @"iPlop", @"maczydeco", @"rileytestut", @"dchavezlive", @"Malvix_", @"wj82315", @"vanillastar67"];
+    NSArray *screenNames = @[@"angelXwind", @"iPlop", @"maczydeco", @"rileytestut", @"dchavezlive", @"Malvix_", @"wj82315", @"vanillastar67", @"winocm"];
     
     if (indexPath.section == 1 && indexPath.row < screenNames.count)
     {
